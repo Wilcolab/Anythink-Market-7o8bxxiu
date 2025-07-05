3c36e3f406cfd48e54d861a4e4722a25fc240cc6
AKSHAMA <123798151+Akshama2003@users.noreply.github.com>
2025-07-05 19:54:41 +0530

    Create basic_prompt.js

---
+++ basic_prompt.js
@@ -0,0 +1,3 @@
+Write a function that converts a string to camelCase.
+For example, the string "hello world" should become "helloWorld".
+Another example: "convert to camel case" should become "convertToCamelCase".
